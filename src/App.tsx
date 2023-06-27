98c0c6a25f88f986d1f5ed3bf6958725276bb6dc
Vishav Ranta <vranta@covalience.com>
2023-06-27 13:32:40 +0530

    Repo initilized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h3 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        React Best Practices
+      </h3>
+    </div>
+  );
+}
+
+export default App;
