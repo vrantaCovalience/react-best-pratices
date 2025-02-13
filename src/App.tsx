2b9813ce56148eedb2d84b84683b7a9ea53da3e1
VISHAV RANTA <135298704+vrantaCovalience@users.noreply.github.com>
2025-02-13 09:53:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <h3 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-        React Best Practices
+        React Best Practices testing
       </h3>
     </div>
   );
